b56172e5c3354559418440f1a941473d78585138
deniseob3 <obried40@tcd.ie>
2022-03-30 17:52:19 +0100

    Remaining stop attributes

--- AlgorithmsBusStops/Stops.java
+++ AlgorithmsBusStops/Stops.java
@@ -1,7 +1,29 @@
 
 public class Stops {
-	public int stopId;
-	public int stopName;
+	public int id;
+	public int code;
+	public String name;
+	public String description;
+	public double latitude;
+	public double longitude;
+	public int zoneID;
+	//public int url; //type of url??
+	public int locationType;
+	//public int parentStation;
+	
+	Stops(int id, int code, String name, String description, double latitude, double longitude, int zoneID, int locationType)
+	{
+		//creating a stop
+		this.id = id;
+		this.code = code;
+		this.name = name;
+		this.description = description;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.zoneID = zoneID;
+		this.locationType = locationType;
+	}
+	
 	
 	
 }

1c0570a65a6bac6363019157bf9e7121d3fbb285
deniseob3 <obried40@tcd.ie>
2022-03-30 18:02:18 +0100

    Started setting up reading the input files

--- AlgorithmsBusStops/ArrivalTimes.java
+++ AlgorithmsBusStops/ArrivalTimes.java
@@ -1,5 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class ArrivalTimes {
+	//class variables
+	
+	ArrivalTimes(String filename)
+	{
+		//read in stop times file and assign it to a 2d array
+		if (filename !=null && filename != "")
+		{
+			try {
+				File file = new File(filename);
+				Scanner scanner = new Scanner(file);
+				int i = 0;
+
+				while(scanner.hasNextLine())
+				{
+					String [] line = scanner.nextLine().trim().split(",");
+					//split each input line by commas
+					//How big is the 2d array??
+				}
+				
+			}
+			catch (Exception e)
+			{
+				System.out.print("Invalid file name");
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

f15e822fb9146be7ed469215c46556450c05c4ee
deniseob3 <obried40@tcd.ie>
2022-04-01 20:19:29 +0100

    Setting up transfers class

---
+++ AlgorithmsBusStops/Transfers.java
@@ -0,0 +1,23 @@
+
+public class Transfers {
+	public int fromStopID;
+	public int toStopID;
+	public int transferType;
+	public int transferTime;
+	
+	Transfers(int fromStopID, int toStopID, int transferType, int transferTime)
+	{
+		this.fromStopID = fromStopID;
+		this.toStopID = toStopID;
+		this.transferType = transferType;
+		this.transferTime = transferTime;
+				
+				
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

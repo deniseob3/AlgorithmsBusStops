a9f7373f5124241135753c1cc41e5fbe0c827eb2
deniseob3 <obried40@tcd.ie>
2022-04-02 11:54:05 +0100

    committing again

--- AlgorithmsBusStops/ArrivalTimes.java
+++ AlgorithmsBusStops/ArrivalTimes.java
@@ -23,7 +23,7 @@ public class ArrivalTimes {
 				File file = new File(filenameStops);
 				Scanner scanner = new Scanner(file);
 				int i = 0;
-
+					//committing
 				while(scanner.hasNextLine())
 				{
 					String [] line = scanner.nextLine().trim().split(",");
